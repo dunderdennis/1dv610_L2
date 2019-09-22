9fba3218ac37b1a7949c8582451c7dcce5de6adf
Dennis Fredsson <dennis.fredsson@hotmail.com>
2019-09-22 16:56:09 +0200

    added clock to bottom of page. saving progress

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -1,12 +1,16 @@
 <?php
 
-class DateTimeView {
+class DateTimeView
+{
 
 
-	public function show() {
+	public function show()
+	{
+		$timeString = date('l, \t\h\e jS \o\f F o, \t\h\e \t\i\m\e \i\s G:i:s');
 
-		$timeString = 'TODO, Write servertime here...';
+		// Sunday, the 22nd of September 2019, The time is 16:39:54
 
 		return '<p>' . $timeString . '</p>';
 	}
 }
+
