dedfe7ddd3edebd25c76ba72f04e5c8617a07733
Dennis Fredsson <dennis.fredsson@hotmail.com>
2019-09-24 00:47:59 +0200

    heroku

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -6,7 +6,7 @@ class DateTimeView
 {
 	public function show()
 	{
-		$timeString = date('l, \t\h\e jS \o\f F o, \t\h\e \t\i\m\e \i\s G:i:s');
+		$timeString = date('l, \t\h\e jS \o\f F o, \T\h\e \t\i\m\e \i\s G:i:s');
 
 		return '<p>' . $timeString . '</p>';
 	}
