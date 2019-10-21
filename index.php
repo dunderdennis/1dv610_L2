ad3aa78de4cb9df84e719337b82742774e665e60
Dennis Fredsson <dennis.fredsson@hotmail.com>
2019-10-21 04:51:34 +0200

    removed passwords from database, so they are not on GitHub

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 // FOR DEV PURPOSES; TO BE DISABLED ON A PUBLIC SERVER
-// error_reporting(E_ALL);
-// ini_set('display_errors', 'On');
-// ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+ini_set('display_startup_errors', 1);
 
 date_default_timezone_set('Europe/Stockholm');
 
